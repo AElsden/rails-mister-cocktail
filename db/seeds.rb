b6b7263cd82f7caaff3c8ccfd512d38481b145bf
Alex Elsden <alex.elsden@hotmail.co.uk>
2019-02-21 15:02:07 +0000

    index, veiw, and create functions added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,22 @@
+require 'json'
+require 'open-uri'
+
+puts 'Cleaning Data Base'
+Ingredient.destroy_all
+puts 'Creating Ingredients...'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_api = open(url).read
+full_hash = JSON.parse(ingredient_api)
+drinks_array = full_hash['drinks']
+sample = drinks_array
+sample.each do |drink|
+  Ingredient.create(name: drink['strIngredient1'])
+  p drink['strIngredient1']
+end
+
+puts 'Finshed..'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
